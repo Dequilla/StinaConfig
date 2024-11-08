d7f55c8166c6a579a9d1fc7922cd98e51e8b21a6
Edwin Wallin <e@edwinwallin.com>
2024-11-09 00:20:45 +0100

    Started on tokenizer.

---
+++ stina-parser/src/tokenizer.cpp
@@ -0,0 +1,13 @@
+#include "tokenizer.hpp"
+
+namespace stina
+{
+    std::vector<token::Token> tokenize(std::string data)
+    {
+        std::vector<token::Token> tokens;
+        for(char c : data)
+        {
+            
+        }
+    }
+}
